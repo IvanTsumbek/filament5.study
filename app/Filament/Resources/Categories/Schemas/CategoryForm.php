2aabc2868005cebade0a0f417ddedccf8930901a
IvanTsumbek <maincumbek@gmail.com>
2026-03-15 19:00:36 +0200

    lesson 3

--- app/Filament/Resources/Categories/Schemas/CategoryForm.php
+++ app/Filament/Resources/Categories/Schemas/CategoryForm.php
@@ -2,10 +2,13 @@
 
 namespace App\Filament\Resources\Categories\Schemas;
 
+use Filament\Forms\Components\DatePicker;
 use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Textarea;
 use Filament\Schemas\Schema;
+use Illuminate\Support\HtmlString;
 
 class CategoryForm
 {
@@ -14,14 +17,49 @@ class CategoryForm
         return $schema
             ->components([
                 TextInput::make('title')
-                    ->required(),
+                    ->default('Test article')
+                    ->helperText(new HtmlString('Helper text for <strong>title</strong>'))
+                    ->hint('Hint for title')
+                    ->hintIcon('heroicon-m-language', 'Tooltip for title')
+                    ->hintColor('primary')
+                    // ->disabled()
+                    ->disabledOn('edit')
+                    ->hiddenOn('edit')
+                    ->autofocus()
+                    ->label('Наименование')
+                    ->required()
+                    ->columnSpan(2),
                 TextInput::make('slug')
                     ->required(),
+                // TextInput::make('email')->email(),
+                Select::make('status')->options([
+                    'Draft', 'Published', 'Reviewing'
+                ])
+                // ->native(false)
+                // ->searchable()
+                ->multiple(),
+                DatePicker::make('published_at')
+                ->native(false)
+                ->displayFormat('d M Y')                   //то, как отображаем
+                ->locale('ru')
+                ->format('Y-m-d')                          //то, что летит на сервер
+                ->minDate(now()->subDays(7))
+                ->maxDate(now()->addDays(7))
+                ->closeOnDateSelection(),                 //автозакрывание календаря                      
+                TextInput::make('email')->type('email'),  //аналог метода выше
+                TextInput::make('password')->password()->revealable(),
+                TextInput::make('phone')->tel()->placeholder('+xx xxx xxx-xx-xx')->mask('+99 999 999-99-99'),
+                TextInput::make('domain')
+                    ->prefix('https://')
+                    ->suffix('.com')
+                    ->suffixIcon('heroicon-m-globe-alt')
+                    ->suffixIconColor('success'),
                 Textarea::make('content')
                     ->required()
                     ->columnSpanFull(),
                 FileUpload::make('image')
                     ->image(),
-            ]);
+            ])
+            ->columns(2);
     }
 }
