aa28377a3a5c05eb835d68b56b67f8fcfff09ba0
IvanTsumbek <maincumbek@gmail.com>
2026-04-05 18:30:49 +0300

    lesson 19: widgets

---
+++ app/Filament/Widgets/LatestOrders.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\Order;
+use Filament\Actions\BulkActionGroup;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Filament\Widgets\TableWidget;
+class LatestOrders extends TableWidget
+{
+    protected int | string | array $columnSpan = 'full';
+    protected static ?int $sort = 3;
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->query(Order::query()->latest())
+            ->defaultPaginationPageOption(1)
+            ->paginated([1, 5, 10])
+            ->columns([
+                TextColumn::make('user_id')
+                    ->numeric()
+                    ->sortable(),
+                TextColumn::make('name')
+                    ->searchable(),
+                TextColumn::make('email')
+                    ->label('Email address')
+                    ->searchable(),
+                TextColumn::make('phone')
+                    ->searchable(),
+                TextColumn::make('status')
+                    ->badge(),
+                TextColumn::make('total')
+                    ->numeric()
+                    ->sortable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->headerActions([
+                //
+            ])
+            ->recordActions([
+                //
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    //
+                ]),
+            ]);
+    }
+}
---
+++ app/Filament/Widgets/StatsOverview.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\Order;
+use App\Models\Product;
+use App\Models\User;
+use Filament\Widgets\StatsOverviewWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+
+class StatsOverview extends StatsOverviewWidget
+{
+    protected ?string $heading = 'Analytics';
+
+    protected ?string $description = 'An overview of some analytics.';
+
+    protected static ?int $sort = 1;
+
+    protected function getStats(): array
+    {
+        return [
+            Stat::make('Users', User::count())
+                ->icon('heroicon-o-users')
+                ->description('32k increase')
+                ->descriptionIcon('heroicon-m-arrow-trending-up')
+                ->chart([7, 2, 10, 3, 15, 4, 17])
+                ->color('success'),
+            Stat::make('Products', Product::count())
+                ->icon('heroicon-o-table-cells'),
+            Stat::make('Orders', Order::count()),
+        ];
+    }
+}
